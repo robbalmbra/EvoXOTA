0e981d896b33bfeb909c80c44dcfda21562653b9
Robert Balmbra <robbalmbra@gmail.com>
2020-04-25 12:47:10 +0100

    Update process.py

--- process.py
+++ process.py
@@ -54,12 +54,12 @@ def process_file(filename,device,output_folder,sf_repo,sf_uname):
   # Create folder structure for device
   project = os.path.basename(os.path.normpath(sf_repo))
   
-  device_folder = os.path.join("/home/frs/project/",str(project),"devices",str(device),str(date))         
-  os.system("ssh " + sf_uname + "@frs.sourceforge.net mkdir -p " + device_folder)
+  #device_folder = os.path.join("/home/frs/project/",str(project),"devices",str(device),str(date))         
+  #os.system("ssh " + sf_uname + "@frs.sourceforge.net mkdir -p " + device_folder)
   
   # Copy file to created directory
-  print("Uploading " + filename.replace(".zip.json",".zip") + " to " + device_folder)
-  os.system("scp " + filename.replace(".zip.json",".zip") + " " + sf_uname + "@frs.sourceforge.net:" + device_folder)
+  #print("Uploading " + filename.replace(".zip.json",".zip") + " to " + device_folder)
+  #os.system("scp " + filename.replace(".zip.json",".zip") + " " + sf_uname + "@frs.sourceforge.net:" + device_folder)
 
 # Checks
 if len(sys.argv) < 5:
