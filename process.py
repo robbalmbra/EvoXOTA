d43a9e0a2582be65be73de1ebf986f2546a82644
Robert Balmbra <robbalmbra@gmail.com>
2020-04-26 09:04:47 +0100

    Update process.py

--- process.py
+++ process.py
@@ -57,7 +57,7 @@ def process_file(filename,device,output_folder,sf_repo,sf_uname):
   device_folder = os.path.join("/home/frs/project/",str(project),"files","devices",str(device),str(date))
   
   print(device_folder)
-  #os.system("ssh -o \"StrictHostKeyChecking no\" " + sf_uname + "@frs.sourceforge.net mkdir -p " + device_folder)
+  os.system("ssh -o \"StrictHostKeyChecking no\" " + sf_uname + "@frs.sourceforge.net mkdir -p " + device_folder)
   
   # Copy file to created directory
   #print("Uploading " + filename.replace(".zip.json",".zip") + " to " + device_folder)
